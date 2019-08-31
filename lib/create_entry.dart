ec8e38e525ea1f405eb31604a5a9c12c58c2a419
Mikael Lindlof <mikuz.dev@gmail.com>
2019-08-31 03:32:20 +0100

    Add QR code scanning.

--- lib/create_entry.dart
+++ lib/create_entry.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:free_authenticator/totp.dart';
 import 'package:free_authenticator/entry.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 class CreateEntry extends StatefulWidget {
   CreateEntry({Key key, this.onCreate}) : super(key: key);
@@ -39,6 +40,24 @@ class _CreateEntry extends State<CreateEntry> {
             final entry = TOTP(nameInput.text, secretInput.text);
             await widget.onCreate(entry);
             Navigator.of(context).pop();
+          },
+        ),
+        new FlatButton(
+          child: new Text('Scan'),
+          onPressed: () async {
+            String otpauth = await BarcodeScanner.scan();
+            var uri = Uri.parse(otpauth);
+            if (uri.scheme != 'otpauth') {
+              print("QR code is not a 2FA secret");
+              return;
+            }
+            print("host " + uri.host);
+            if (uri.host != 'totp') {
+              print("Only TOTP is supported");
+              return;
+            }
+            nameInput.text = uri.pathSegments[0];
+            secretInput.text = uri.queryParameters['secret'];
           },
         ),
         new FlatButton(
