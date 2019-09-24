39171f4b312124a28e07f9fc252b0782f707ce03
Mikael Lindlof <mikuz.dev@gmail.com>
2019-09-24 22:47:27 +0100

    #6 Fix test app reference.

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,19 @@ import 'package:free_authenticator/entry_list.dart';
 import 'package:free_authenticator/vault.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Free Authenticator',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: EntryList(title: 'One-time Passwords', vaultId: Vault.rootId),
-  ));
+  runApp(FreeAuthenticatorApp());
+}
+
+class FreeAuthenticatorApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Free Authenticator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: EntryList(title: 'One-time Passwords', vaultId: Vault.rootId),
+    );
+  }
 }
