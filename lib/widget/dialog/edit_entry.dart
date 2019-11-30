ed66b70d510a22ebd8df2a2350d9455a7a508192
Mikael Lindlof <mikuz.dev@gmail.com>
2019-11-30 00:36:46 +0000

    #10 Restrict vault from being nested

--- lib/widget/dialog/edit_entry.dart
+++ lib/widget/dialog/edit_entry.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:free_authenticator/model/interface/entry.dart';
+import 'package:free_authenticator/model/interface/entry_type.dart';
 import 'package:free_authenticator/widget/dialog/vault_field.dart';
 import 'package:free_authenticator/widget/store_injector.dart';
 
@@ -27,30 +28,41 @@ class _EditEntry extends State<EditEntry> {
     this.nameInput.text = this.widget.entry.name;
   }
 
+  bool _showVaultInput() => this.widget.entry.type != EntryType.vault;
+  Future<int> _getVault() async {
+    if (!this._showVaultInput()) return null;
+    else if (vaultInput.text == "") return VaultEntry.rootId;
+    return await StoreInjector.of(context).getOrCreateVault(vaultInput.text);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var inputFields = (
+      <Widget>[
+            TextField(
+              controller: nameInput,
+              decoration: InputDecoration(hintText: "Name"),
+            ),
+      ]);
+    if (this._showVaultInput()) {
+      inputFields.add(
+        VaultField(
+          controller: vaultInput,
+          decoration: InputDecoration(hintText: "Vault"),
+          entry: this.widget.entry,
+        ));
+    }
     return AlertDialog(
       title: Text('Enter a secret'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextField(
-            controller: nameInput,
-            decoration: InputDecoration(hintText: "Name"),
-          ),
-          VaultField(
-            controller: vaultInput,
-            decoration: InputDecoration(hintText: "Vault"),
-            entry: this.widget.entry,
-          ),
-        ],
+        children: inputFields,
       ),
       actions: <Widget>[
         new FlatButton(
           child: new Text('Ok'),
           onPressed: () async {
-            int vault = vaultInput.text == "" ? VaultEntry.rootId :
-              await StoreInjector.of(context).getOrCreateVault(vaultInput.text);
+            int vault = await this._getVault();
             await StoreInjector.of(context).updateEntry(this.widget.entry, name: nameInput.text, vault: vault);
             Entry entry = await StoreInjector.of(context).getEntry(this.widget.entry.id);
             Navigator.of(context).pop();
