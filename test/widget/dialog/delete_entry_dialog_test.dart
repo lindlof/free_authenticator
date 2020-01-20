f2af2e3eeba28c59f37a492c723c2e2ef4f3e9ad
Mikael Lindlof <mikuz.dev@gmail.com>
2020-01-20 01:31:11 +0000

    #1 Test delete dialog

---
+++ test/widget/dialog/delete_entry_dialog_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:free_authenticator/model/api/entry.dart';
+import 'package:free_authenticator/model/api/entry_type.dart';
+import 'package:free_authenticator/widget/dialog/delete_entry_dialog.dart';
+
+import 'package:free_authenticator/widget/entry_list.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../mock/mock_store.dart';
+import '../../parentWidget/main_test_widget.dart';
+
+const int PUMP_DURATION_MS = 10;
+
+class MockDialogs extends Mock implements Dialogs {}
+
+void main() {
+  testWidgets('Delete entry', (WidgetTester tester) async {
+    final store = MockStore();
+    final vaultName = "Redundant vault";
+    final secret = "123456";
+
+    await store.createEntry(EntryType.totp, VaultEntry.rootId, name: vaultName, secret: secret, timestep: 30);
+    final Entry entry = await store.getEntry(2);
+
+    expect(entry, isNotNull, reason: "Entry id 2 missing before deletion");
+
+    await tester.pumpWidget(MainTestWidget(DeleteEntryDialog(entry: entry), store: store));
+    await tester.pump(Duration(milliseconds: PUMP_DURATION_MS));
+
+    // Make sure cancel reads cancel
+    await tester.tap(find.widgetWithText(FlatButton, "Delete"));
+
+    var entryAfter;
+    try { entryAfter = await store.getEntry(2); }
+    catch (e) { }
+    expect(entryAfter, isNull, reason: "Entry id 2 exists after deletion");
+  });
+
+  testWidgets('Cancel delete', (WidgetTester tester) async {
+    final store = MockStore();
+    final vaultName = "Important vault";
+    final secret = "123456";
+
+    await store.createEntry(EntryType.totp, VaultEntry.rootId, name: vaultName, secret: secret, timestep: 30);
+    final Entry entry = await store.getEntry(2);
+
+    await tester.pumpWidget(MainTestWidget(DeleteEntryDialog(entry: entry), store: store));
+    await tester.pump(Duration(milliseconds: PUMP_DURATION_MS));
+
+    // Make sure cancel reads cancel
+    await tester.tap(find.widgetWithText(FlatButton, "Cancel"));
+
+    var entryAfter;
+    try { entryAfter = await store.getEntry(2); }
+    catch (e) { }
+    expect(entryAfter, isNotNull, reason: "Entry id 2 missing after canceling deletion");
+  });
+}
