8aba0e5875563d8150902f6c54eb7918c9b1ee43
Mikael Lindlof <mikuz.dev@gmail.com>
2020-01-26 02:27:09 +0000

    #1 Test database provider

---
+++ test/database/database_provider_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:free_authenticator/database/database_entry.dart';
+import 'package:free_authenticator/database/database_provider.dart';
+import 'package:free_authenticator/keychain/keychain_provider.dart';
+import 'package:free_authenticator/model/api/entry.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class MockDatabase extends Mock implements Database {}
+class MockKeychainProvider extends Mock implements KeychainProvider {}
+class MockDeps extends Mock implements DatabaseProviderDeps {}
+
+void main() {
+
+  testWidgets('Provides database', (WidgetTester tester) async {
+    final database = MockDatabase();
+    final keychain = MockKeychainProvider();
+    final dependencies = MockDeps();
+
+    when(dependencies.openDatabase(any, version: anyNamed("version"), onCreate: anyNamed("onCreate")))
+      .thenAnswer((invocation) async {
+        return database;
+      });
+
+    final provider = DatabaseProvider(keychain, dependencies: dependencies);
+    Database providedDatabase = await provider.database;
+
+    expect(providedDatabase, equals(database));
+  });
+
+  testWidgets('Database creation', (WidgetTester tester) async {
+    final database = MockDatabase();
+    final keychain = MockKeychainProvider();
+    final dependencies = MockDeps();
+
+    when(dependencies.openDatabase(any, version: anyNamed("version"), onCreate: anyNamed("onCreate")))
+      .thenAnswer((invocation) async {
+        Function(Database, int) onCreate = invocation.namedArguments[Symbol("onCreate")];
+        await onCreate(database, 1);
+        return database;
+      });
+
+    final provider = DatabaseProvider(keychain, dependencies: dependencies);
+    await provider.database;
+
+    verify(database.execute(argThat(contains('CREATE TABLE ${DatabaseEntry.table}'))));
+    verify(database.insert(DatabaseEntry.table, argThat(containsPair(DatabaseEntry.columnId, "${VaultEntry.rootId}"))));
+  });
+}
